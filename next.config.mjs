01154c81b24406b3d034742552059f85e42a7a59
hyochan <dooboolab@gmail.com>
2024-04-15 10:55:01 +0900

    fix: cors policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "gpt.hyochan.dev",
-        port: '',
+        port: "",
         pathname: "/**",
       },
     ],
