9e5720f854e0bf409af16a5a14c2a2e5266239bc
hyochan <dooboolab@gmail.com>
2024-04-28 19:54:08 +0900

    fix: main tag overflow scroll-y

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ export default async function Home() {
   return (
     <div className="h-screen flex flex-col">
       <Header />
-      <main className={clsx(
-        "py-12 px-4 gap-4",
-        "flex flex-1 self-stretch flex-col",
-        "md:px-24"
-      )}>
+      <main
+        className={clsx(
+          "py-12 px-4 gap-4 overflow-y-scroll",
+          "flex flex-1 self-stretch flex-col",
+          "md:px-24"
+        )}
+      >
         <div className="flex flex-row items-center gap-4 self-center mb-4">
           <Image src="/icon.png" alt="App Icon" width={36} height={36} />
           <h1 className="text-3xl text-white">Hyo GPT</h1>
